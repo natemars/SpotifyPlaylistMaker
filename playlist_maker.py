8494ddcee4bfd823571aa6b0ff6870a69ce13542
unknown <nathan.r.marshall@usm.edu>
2020-08-30 08:17:35 -0500

    Added user auth token version of spotiy api

--- playlist_maker.py
+++ playlist_maker.py
@@ -1,18 +1,18 @@
-from secrets import client_id, client_secret, user_auth_token
-from spotify_api import SpotifyAPI
+from secrets import user_auth_token
+from spotify_api_oauth import SpotifyAPI
 import json
 
-spotify = SpotifyAPI(client_id, client_secret)
-# print(spotify.search({"track": "Time"}))
-json_output = spotify.get_playlists(user_auth_token)
-# print(json.dumps(json_output, indent=4))
-for key in json_output:
-    # print(json.dumps(json_output[key], indent=4))
-    if (key == "items"):
-        # print(json.dumps(json_output[key], indent=4))
-        for item in json_output[key]:
-            # print(json.dumps(item, indent=4))
-            for item_key in item:
-                if (item_key == "name"):
-                    print(item[item_key])
 
+spotify = SpotifyAPI(user_auth_token)
+# print(spotify.search({"track": "Time"}))
+json_output = spotify.get_playlists()
+print(json.dumps(json_output, indent=4))
+# for key in json_output:
+#     # print(json.dumps(json_output[key], indent=4))
+#     if (key == "items"):
+#         # print(json.dumps(json_output[key], indent=4))
+#         for item in json_output[key]:
+#             # print(json.dumps(item, indent=4))
+#             for item_key in item:
+#                 if (item_key == "name"):
+#                     print(item[item_key])
--- spotify_api_oauth.py
+++ spotify_api_oauth.py
@@ -2,79 +2,18 @@ import requests
 import datetime
 from urllib.parse import urlencode
 
-
 import base64
 
+
 class SpotifyAPI(object):
-    access_token = None
-    access_token_expires = datetime.datetime.now()
-    access_token_did_expire = True
-    client_id = None
-    client_secret = None
-    token_url = "https://accounts.spotify.com/api/token"
+    user_auth_token = None
 
-    def __init__(self, client_id, client_secret, *args, **kwargs):
+    def __init__(self, user_auth_token, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.client_id = client_id
-        self.client_secret = client_secret
-
-    def get_client_credentials(self):
-        """
-        Returns a base64 encoded string
-        """
-        client_id = self.client_id
-        client_secret = self.client_secret
-        if client_secret == None or client_id == None:
-            raise Exception("You must set client_id and client_secret")
-        client_creds = f"{client_id}:{client_secret}"
-        client_creds_b64 = base64.b64encode(client_creds.encode())
-        return client_creds_b64.decode()
-
-    def get_token_headers(self):
-        client_creds_b64 = self.get_client_credentials()
-        return {
-            "Authorization": f"Basic {client_creds_b64}"
-        }
-
-    def get_token_data(self):
-        return {
-            "grant_type": "client_credentials"
-        }
-
-    def perform_auth(self):
-        token_url = self.token_url
-        token_data = self.get_token_data()
-        token_headers = self.get_token_headers()
-        r = requests.post(token_url, data=token_data, headers=token_headers)
-        if r.status_code not in range(200, 299):
-            raise Exception("Could not authenticate client.")
-            # return False
-        data = r.json()
-        now = datetime.datetime.now()
-        access_token = data['access_token']
-        expires_in = data['expires_in']  # seconds
-        expires = now + datetime.timedelta(seconds=expires_in)
-        self.access_token = access_token
-        self.access_token_expires = expires
-        self.access_token_did_expire = expires < now
-        return True
-
-    def get_access_token(self):
-        token = self.access_token
-        expires = self.access_token_expires
-        now = datetime.datetime.now()
-        if expires < now:
-            self.perform_auth()
-            return self.get_access_token()
-        elif token == None:
-            self.perform_auth()
-            return self.get_access_token()
-
-        print("token: "+token)
-        return token
+        self.user_auth_token = user_auth_token
 
     def get_resource_header(self):
-        access_token = self.get_access_token()
+        access_token = self.user_auth_token
         headers = {
             "Authorization": f"Bearer {access_token}"
         }
@@ -103,13 +42,11 @@ class SpotifyAPI(object):
             return {}
         return r.json()
 
-    def get_playlists(self, access_token, offset=0, limit=5):
-        headers = {
-            "Authorization": f"Bearer {access_token}"
-        }
+    def get_playlists(self, offset=0, limit=5):
         endpoint = "https://api.spotify.com/v1/me/playlists"
         query_params = urlencode({"offset": offset, "limit": limit})
         get_playlists_url = f"{endpoint}?{query_params}"
+        headers = self.get_resource_header()
         r = requests.get(get_playlists_url, headers=headers)
         print("Playlist url: " + get_playlists_url)
         if r.status_code not in range(200, 299):
